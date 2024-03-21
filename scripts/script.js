41f914e1bf28e1211d13a9a7b1c9796cbae1729b
João Pedro Resende Silva <90474257+notprQn@users.noreply.github.com>
2024-03-21 11:17:30 -0300

    Refactor camera view and annotation functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-const modelViewer2 = document.querySelector("#hotspot-camera-view-demo");
+const cameraView = document.querySelector("#hotspot-camera-view-demo");
 const annotationClicked = (annotation) => {
 let dataset = annotation.dataset;
-    modelViewer2.cameraTarget = dataset.target;
-    modelViewer2.cameraOrbit = dataset.orbit;
-    modelViewer2.fieldOfView = '45deg';
+    cameraView.cameraTarget = dataset.target;
+    cameraView.cameraOrbit = dataset.orbit;
+    cameraView.fieldOfView = '45deg';
 }
 
-modelViewer2.querySelectorAll('button').forEach((hotspot) => {
+cameraView.querySelectorAll('button').forEach((hotspot) => {
     hotspot.addEventListener('click', () => annotationClicked(hotspot));
 });
